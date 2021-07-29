a420365e89a11ab1e835df82ead6696d6715e513
Mainul Islam <mainul.islam@mulesoft.com>
2021-07-29 12:38:15 +1000

    Updated version

--- apimanagerutil.py
+++ apimanagerutil.py
@@ -12,45 +12,68 @@ def main():
     api_name = sys.argv[4]
     group_id = sys.argv[5]
     env = sys.argv[6]
-    version_overwrite = sys.argv[7]
-    major_version_overwrite = sys.argv[8]
+    
 
 
     access_token = autheticate_with_anypoint_platform(username,password)
-    
-    #exchange_api_version, exchange_api_version_group = fetch_exchange_details(access_token, group_id, api_name)
-    exchange_api_version = '1.0.1'
-    exchange_api_version_group = 'v1'
-    apis = fetch_apimanager_details(access_token, group_id, env, api_name)
-    #print(apis)
-    updateFlag = True
-    apiId = 0
-    needToUpdate = True
 
-    if version_overwrite == 'NORMAL':
-        if apis != []:
-            for item in apis:
-                if item['productVersion'] == exchange_api_version_group and item['assetVersion'] > exchange_api_version:
-                    #print('Higher version found in API manager, Not updating')
-                    updateFlag = False
-                    break
-                else:
-                    apiId = item['id']
-        else: 
-            #print("New API")
-            needToUpdate = False
+    if operation_type == 'GETID': # Getting API from api manager
+        
+        version_overwrite = sys.argv[7]
+        major_version_overwrite = sys.argv[8]
+        #exchange_api_version, exchange_api_version_group = fetch_exchange_details(access_token, group_id, api_name)
+        exchange_api_version = '1.0.1'
+        exchange_api_version_group = 'v1'
+        apis = fetch_apimanager_details(access_token, group_id, env, api_name)
+        #print(apis)
+        updateFlag = True
+        apiId = 0
+        needToUpdate = True
 
-        if apiId == 0:
-            #print('Major version update')
-            needToUpdate = False
+        if version_overwrite == 'NORMAL':
+            if apis != []:
+                for item in apis:
+                    if item['productVersion'] == exchange_api_version_group and item['assetVersion'] > exchange_api_version:
+                        #print('Higher version found in API manager, Not updating')
+                        updateFlag = False
+                        break
+                    else:
+                        apiId = item['id']
+            else: 
+                #print("New API")
+                needToUpdate = False
+
+            if apiId == 0:
+                #print('Major version update')
+                needToUpdate = False
+        
+        message = {'api_id': apiId, 'updateVersion': needToUpdate, 'exchageVersion':exchange_api_version}
     
-    message = {'api_id': apiId, 'updateVersion': needToUpdate}
+    elif operation_type == 'UPDATEVERSION':
+        apiId = sys.argv[7]
+        newVersion = sys.argv[8]
+        message = update_api_manager_version(access_token, group_id, env, apiId, newVersion)
+
     print(message)
     return 0
 
 
 
-    
+def update_api_manager_version(access_token, group_id, env, api_id, new_version):    
+    url_update_apimanager = "https://anypoint.mulesoft.com/apimanager/api/v1/organizations/" + group_id \
+            + "/environments/" + env + "/apis/" + api_id
+    headers={"authorization": "Bearer "+ access_token}
+    message = {'assetVersion':new_version}
+    response = requests.patch(url_update_apimanager,headers = headers,data = message)
+
+    if response.status_code != 200:
+        #return 'ERROR Updating API manager'
+        raise Exception('ERROR Updating API manager')
+    else:
+        return 'API manager Update SUCCESS'
+
+
+
 
 
 def fetch_apimanager_details(access_token, group_id, env, api_name):
