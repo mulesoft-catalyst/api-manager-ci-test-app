996e0a8c4ffdf0589ce662c5ad32251bfd432113
Mainul Islam <mainul.islam@mulesoft.com>
2021-07-29 16:31:12 +1000

    Happy path version

--- apimanagerutil.py
+++ apimanagerutil.py
@@ -21,9 +21,9 @@ def main():
         
         version_overwrite = sys.argv[7]
         major_version_overwrite = sys.argv[8]
-        #exchange_api_version, exchange_api_version_group = fetch_exchange_details(access_token, group_id, api_name)
-        exchange_api_version = '1.0.1'
-        exchange_api_version_group = 'v1'
+        exchange_api_version, exchange_api_version_group = fetch_exchange_details(access_token, group_id, api_name)
+        #exchange_api_version = '1.0.0'
+        #exchange_api_version_group = 'v1'
         apis = fetch_apimanager_details(access_token, group_id, env, api_name)
         #print(apis)
         updateFlag = True
@@ -41,6 +41,7 @@ def main():
                         apiId = item['id']
             else: 
                 #print("New API")
+                apiId = add_api_manager(access_token, group_id, env, api_name, exchange_api_version)
                 needToUpdate = False
 
             if apiId == 0:
@@ -57,6 +58,21 @@ def main():
     print(message)
     return 0
 
+
+def add_api_manager(access_token, group_id, env, api_name, api_version):
+    url_insert_apimanager = "https://anypoint.mulesoft.com/apimanager/api/v1/organizations/" + group_id \
+            + "/environments/" + env + "/apis"
+    headers={"authorization": "Bearer "+ access_token, 'Content-Type': 'application/json'}
+
+    message = {"endpoint": {"deploymentType": "CH", "isCloudHub": None,"muleVersion4OrAbove": True,"proxyUri": None,"referencesUserDomain": None,"responseTimeout": None,"type": "raml","uri": None}, "providerId": None,"instanceLabel": None,"spec": {"assetId": api_name,"groupId": group_id,"version": api_version}}
+    response = requests.post(url_insert_apimanager,headers = headers,data = json.dumps(message))
+
+
+    if response.status_code > 399:
+        raise Exception("Could not create API instance in API manager")
+
+    data = response.json()
+    return data['id']
 
 
 def update_api_manager_version(access_token, group_id, env, api_id, new_version):    
@@ -104,10 +120,18 @@ def fetch_apimanager_details(access_token, group_id, env, api_name):
 def fetch_exchange_details(access_token, group_id, api_name):
     
     url_exchange = "https://anypoint.mulesoft.com/exchange/api/v2/assets/" + group_id + "/" + api_name
+    headers={"authorization": "Bearer "+ access_token}
+    response = requests.get(url_exchange,headers = headers)
     
-    with open('api-details-exchange.json') as f:
-        data = json.load(f)
+    if response.status_code == 404:
+        #print(response.status_code)
+        raise Exception('Resource was not found in exchange')
+    
+    elif response.status_code != 200:
+        #print(response.status_code)
+        raise Exception('Error during data fetch from Exchange')
 
+    data = response.json()
     api_version = data['version']
     api_version_group = data['versionGroup']
     
@@ -120,13 +144,19 @@ def autheticate_with_anypoint_platform(user, password):
 
     url_login = "https://anypoint.mulesoft.com/accounts/login"
     message = {'username':user,'password':password}
-    headers={"Accept": "application/json"}
+    headers = {"Accept": "application/json", "Content-Type": "application/json"}
 
-    #response = requests.post(url_login,headers = headers,data = message)
-    #access_token = response.json()['access_token']
+    response = requests.post(url_login,headers = headers,data = json.dumps(message))
+    print(response.status_code)
+
+    if response.status_code > 399:
+        #print(response.status_code)
+        raise Exception('Error during Access token fetch')
+
+    access_token = response.json()['access_token']
     #print(access_token)
 
-    return "0dd542aa-5103-4932-85d6-4b2aaa0203a5"
+    return access_token
 
 
 main()
